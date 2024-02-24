e07a6519ef5a5b9e7c2af40ecab02f019ed26614
imanwaheed317 <160356928+imanwaheed317@users.noreply.github.com>
2024-02-24 22:34:30 +0500

    Add files via upload

---
+++ Number_eight.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(1 + 7); //addition
+console.log(14 - 6); //subtraction
+console.log(2 * 4); //multiplication
+console.log(16 / 2); //division
---
+++ Number_eight.ts
@@ -0,0 +1,7 @@
+console.log(1+7); //addition
+
+console.log(14-6); //subtraction
+
+console.log(2*4); //multiplication
+
+console.log(16/2); //division
